62a04fdcabd3b9dc9a36b2eb66584eddb95c29f6
Argha Sen <iamslowdeath@gmail.com>
2022-10-16 11:51:55 +0800

    update object documentation

--- include/Screeps/Object.hpp
+++ include/Screeps/Object.hpp
@@ -4,17 +4,40 @@
 #include "JS.hpp"
 
 namespace Screeps {
-
+    /**
+     * class Object describes a JS object and as such is the main way to interact with JS data
+     */
 class Object
 {
 public:
 	using Error = int;
 
+    /**
+     * get the underlying JS value (non const version)
+     * @return JS value
+     */
 	JS::Value& value();
+    /**
+     * get the underlying JS value(const version)
+     * @return JS value
+     */
 	const JS::Value& value() const;
+    /**
+     * Set the JS Value(as defined by emscripten Val)
+     * @param value the value to set
+     */
 	void setValue(JS::Value value);
 
+    /**
+    * get the underlying JS value (non const version, operator based)
+    * @return JS value
+    */
 	operator JS::Value&();
+
+    /**
+    * get the underlying JS value (const version, operator based)
+    * @return JS value
+    */
 	operator const JS::Value&() const;
 
 protected:
